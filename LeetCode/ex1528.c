83a7ca37ea43247567f0189b3bea99f2f32be75a
Gabriel Schuch <gksgabriel2004@gmail.com>
2025-10-10 14:45:35 -0300

    Exercicio 1528 do LeetCode(Shuffle String)

---
+++ LeetCode/ex1528.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *restoreString(char *s, int *indices, int indicesSize) {
+  char *result = (char *)malloc(sizeof(char) * (indicesSize + 1));
+  for (int i = 0; i < indicesSize; i++)
+    result[indices[i]] = s[i];
+
+  result[indicesSize] = '\0';
+  return result;
+}
+
+int main() {
+  char *s = "codeleet";
+  int indices[] = {4, 5, 6, 7, 0, 2, 1, 3};
+
+  int size = strlen(s);
+
+  char *result = restoreString(s, indices, size);
+
+  if (result != NULL) {
+    printf("%s\n", result);
+
+    free(result);
+  }
+
+  return 0;
+}
