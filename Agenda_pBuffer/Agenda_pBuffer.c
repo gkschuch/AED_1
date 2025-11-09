d8e3c1b721236c10610f96623f1baba1db53fb42
Gabriel Schuch <gksgabriel2004@gmail.com>
2025-11-09 16:27:06 -0300

    Refatora addPerson para alocação exata de memória

--- Agenda_pBuffer/Agenda_pBuffer.c
+++ Agenda_pBuffer/Agenda_pBuffer.c
@@ -2,49 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAME_SIZE sizeof(char) * 20
-#define EMAIL_SIZE sizeof(char) * 30
-#define AGE_SIZE sizeof(int)
-#define PERSON_SIZE (NAME_SIZE + EMAIL_SIZE + AGE_SIZE)
+#define NAME_SIZE 20
+#define EMAIL_SIZE 30
 
-#define INITIAL_SIZE ((sizeof(int) * 6) + PERSON_SIZE)
-/*
-        MEMORY LAYOUT:
-    (int*)pBuffer + 0: pMenu
-    (int*)pBuffer + 1: pCount
-    (int*)pBuffer + 2: pCapacity
-    (int*)pBuffer + 3: pI
-    (int*)pBuffer + 4: pJ (i+1)
-    (int*)pBuffer + 5: pIndexFound
-    Depois disso: Buffers temporários (PERSON_SIZE bytes)
+#define pMenu(pBuffer) ((int *)pBuffer)
+#define pCount(pBuffer) ((int *)pBuffer + 1)
+#define pI(pBuffer) ((int *)pBuffer + 2)
+#define pJ(pBuffer) ((int *)pBuffer + 3)
+#define pIndexFound(pBuffer) ((int *)pBuffer + 4)
+#define pCapacity(pBuffer) ((int *)pBuffer + 5)
+#define pUsed(pBuffer) ((int *)pBuffer + 6)
+#define pTotalPersonSize(pBuffer) ((int *)pBuffer + 7)
 
-    PERSON REGISTER LAYOUT:
-    Name
-    Email
-    Age
-*/
+#define HEADER_INT_COUNT (8)
+#define HEADER_SIZE (sizeof(int) * HEADER_INT_COUNT)
+
+#define pTempStart(pBuffer) ((void *)((int *)pBuffer + HEADER_INT_COUNT))
+#define pTempName(pBuffer) ((char *)pTempStart(pBuffer))
+#define pTempEmail(pBuffer) ((char *)pTempStart(pBuffer) + NAME_SIZE)
+#define pTempAge(pBuffer) ((int *)((char *)pTempEmail(pBuffer) + EMAIL_SIZE))
+
+#define TEMP_AREA_SIZE (NAME_SIZE + EMAIL_SIZE + sizeof(int))
+
+#define DATA_START_OFFSET (HEADER_SIZE + TEMP_AREA_SIZE)
 
 void menu();
+void initBuffer(void **pBufferPtr);
 void addPerson(void **pBufferPtr);
 void removePerson(void *pBuffer);
 void searchPerson(void *pBuffer);
 void listAgenda(void *pBuffer);
 
 int main() {
-  void *pBuffer = malloc(INITIAL_SIZE);
-  if (pBuffer == NULL) {
-    printf("pBuffer: Memory Allocation failed\n");
-    return 1;
-  }
-  memset(pBuffer, 0, INITIAL_SIZE);
+  void *pBuffer;
 
-  *((int *)pBuffer + 1) = 0; // zera o contador de pessoas
-  *((int *)pBuffer + 2) = 0; // zera a capacidade
+  initBuffer(&pBuffer);
   while (1) {
     menu();
     printf("\nInsert a command: ");
-    scanf(" %d", (int *)pBuffer);
-    switch (*(int *)pBuffer) {
+    scanf(" %d", pMenu(pBuffer));
+    switch (*pMenu(pBuffer)) {
     case 1:
       addPerson(&pBuffer);
       break;
@@ -70,6 +67,28 @@ int main() {
   }
 }
 
+void initBuffer(void **pBufferPtr) {
+  *pBufferPtr =
+      malloc(DATA_START_OFFSET); //  espaco para as variaveis estaticas
+
+  // Verificação de falha do malloc
+  if (*pBufferPtr == NULL) {
+    printf("pBuffer: Memory Allocation failed\n");
+    exit(1);
+  }
+  // Seta os valores iniciais
+  *pCapacity(*pBufferPtr) = 0; // seta a capacidade inicial dos dados para 0
+  *pCount(*pBufferPtr) = 0;    // zera o contador de pessoas
+  *pUsed(*pBufferPtr) = 0;     // espaco usado para os dados(inicial = 0)
+  *pTotalPersonSize(*pBufferPtr) = 0;
+
+  // Zera todos as variaveis de controle
+  *(pMenu(*pBufferPtr)) = 0;
+  *(pI(*pBufferPtr)) = 0;
+  *(pJ(*pBufferPtr)) = 0;
+  *(pIndexFound(*pBufferPtr)) = 0;
+}
+
 void menu() {
   printf("-----------AGENDA pBuffer-----------\n\n");
   printf("\t1) ADD PERSON(NAME, EMAIL, AGE)\n");
@@ -83,45 +102,53 @@ void addPerson(void **pBufferPtr) {
   void *pBuffer = *pBufferPtr;
   void *newBuffer;
 
-  //     Count >= Capacity
-  if (*((int *)pBuffer + 1) >= *((int *)pBuffer + 2)) {
-    (*((int *)pBuffer + 2))++; // Capacity++;
-    newBuffer = realloc(pBuffer,
-                        INITIAL_SIZE + ((*((int *)pBuffer + 2)) * PERSON_SIZE));
+  printf("Person to add\n");
+  printf("\tInsert Name: ");
+  scanf(" %[^\n]",
+        pTempName(pBuffer)); // Armazena no espaco temporario a string do nome
+  printf("\tInsert Email: ");
+  scanf(" %[^\n]",
+        pTempEmail(pBuffer)); // Armazena no espaco temporario a string do email
+  printf("\tInsert Age: ");
+  scanf(" %d", pTempAge(pBuffer)); // Armazena no espaco temporario a idade
+
+  //  Layout Calculo: TAMANHO NOME(+ \0) + TAMNHO EMAIL(+ \0) + TAMNHO INTEIRO
+  *pTotalPersonSize(pBuffer) = (strlen(pTempName(pBuffer)) + 1) +
+                               (strlen(pTempEmail(pBuffer)) + 1) + sizeof(int);
+
+  if (*pUsed(pBuffer) + *pTotalPersonSize(pBuffer) > *pCapacity(pBuffer)) {
+    *pCapacity(pBuffer) = *pUsed(pBuffer) + *pTotalPersonSize(pBuffer);
+
+    newBuffer = realloc(pBuffer, *pCapacity(pBuffer));
+
+    // Verificação de falha do realloc
     if (newBuffer == NULL) {
-      printf("pBuffer: Error reallocating\n");
-      (*((int *)pBuffer + 2))--; // Capacity--;
+      printf("pBuffer: Memory Reallocation Failed.");
       return;
     }
     *pBufferPtr = newBuffer;
     pBuffer = newBuffer;
   }
-  void *pTemp = ((int *)pBuffer + 6); // Avança até o campo buffers temporarios
-                                      //   (Nome, email, e idade)
-  printf("Person to add\n");
-  printf("\tInsert Name: ");
-  scanf(" %[^\n]", (char *)pTemp);
-  printf("\tInsert Email: ");
-  scanf(" %[^\n]", (char *)(pTemp + NAME_SIZE));
-  printf("\tInsert Age: ");
-  scanf(" %d", (int *)(pTemp + NAME_SIZE + EMAIL_SIZE));
+  // Copia do nome
+  memcpy((char *)pBuffer + *pUsed(pBuffer), pTempName(pBuffer),
+         strlen(pTempName(pBuffer)) + 1);
+  *pUsed(pBuffer) += strlen(pTempName(pBuffer)) + 1;
 
-  strcpy((char *)(pBuffer + INITIAL_SIZE +
-                  ((*((int *)pBuffer + 1)) * PERSON_SIZE)),
-         pTemp); // Copia Nome
-  strcpy((char *)(pBuffer + INITIAL_SIZE +
-                  ((*((int *)pBuffer + 1)) * PERSON_SIZE) + NAME_SIZE),
-         pTemp + NAME_SIZE); // Copia Email
-  *((int *)(pBuffer + INITIAL_SIZE + ((*((int *)pBuffer + 1)) * PERSON_SIZE) +
-            NAME_SIZE + EMAIL_SIZE)) =
-      *((int *)(pTemp + NAME_SIZE + EMAIL_SIZE));
+  // Copia do email
+  memcpy((char *)pBuffer + *pUsed(pBuffer), pTempEmail(pBuffer),
+         strlen(pTempEmail(pBuffer)) + 1);
+  *pUsed(pBuffer) += strlen(pTempEmail(pBuffer)) + 1;
 
-  (*((int *)pBuffer + 1))++;
+  // Copia da idade
+  memcpy((char *)pBuffer + *pUsed(pBuffer), pTempAge(pBuffer), sizeof(int));
+  *pUsed(pBuffer) += sizeof(int);
+
+  (*pCount(pBuffer))++;
 }
 
 void removePerson(void *pBuffer) {
   //   Check if pCount is equal to 0
-  if ((*((int *)pBuffer + 1)) == 0) {
+  if (*pCount(pBuffer) == 0) {
     printf("ERROR: Agenda is empty\n");
     return;
   }
@@ -131,13 +158,10 @@ void removePerson(void *pBuffer) {
 
   *((int *)pBuffer + 5) = -1; // pIndexFound = -1;
   //   for(int i = 0; i< count;i++);
-  for (*((int *)pBuffer + 3) = 0;
-       (*((int *)pBuffer + 3)) < (*((int *)pBuffer + 1));
-       (*((int *)pBuffer + 3))++) {
-    if (strcmp((char *)pTemp,
-               (char *)(pBuffer + INITIAL_SIZE +
-                        ((*((int *)pBuffer + 3)) * PERSON_SIZE))) == 0) {
-      *((int *)pBuffer + 5) = *((int *)pBuffer + 3); // pIndexFound = pI;
+  for (*pI(pBuffer) = 0; *pI(pBuffer) < *pCount(pBuffer); (*pI(pBuffer))++) {
+    if (strcmp((char *)pTemp, (char *)(pBuffer + INITIAL_SIZE +
+                                       ((*pI(pBuffer)) * PERSON_SIZE))) == 0) {
+      *((int *)pBuffer + 5) = *pI(pBuffer); // pIndexFound = pI;
       break;
     }
   }
@@ -146,25 +170,24 @@ void removePerson(void *pBuffer) {
     return;
   }
 
-  for (*((int *)pBuffer + 3) = *((int *)pBuffer + 5);
-       (*((int *)pBuffer + 3)) < (*((int *)pBuffer + 1)) - 1;
-       (*((int *)pBuffer + 3))++) {
-    void *pDestination = (void *)(pBuffer + INITIAL_SIZE +
-                                  ((*((int *)pBuffer + 3)) * PERSON_SIZE));
-    *((int *)pBuffer + 4) = (*((int *)pBuffer + 3)) + 1;
+  for (*pI(pBuffer) = *((int *)pBuffer + 5);
+       *pI(pBuffer) < *pCount(pBuffer) - 1; (*pI(pBuffer))++) {
+    void *pDestination =
+        (void *)(pBuffer + INITIAL_SIZE + ((*pI(pBuffer)) * PERSON_SIZE));
+    *pJ(pBuffer) = (*pI(pBuffer)) + 1;
 
-    void *pOrigin = (void *)(pBuffer + INITIAL_SIZE +
-                             ((*((int *)pBuffer + 4)) * PERSON_SIZE));
+    void *pOrigin =
+        (void *)(pBuffer + INITIAL_SIZE + ((*pJ(pBuffer)) * PERSON_SIZE));
 
     memcpy(pDestination, pOrigin, PERSON_SIZE);
   }
-  (*((int *)pBuffer + 1))--;
+  (*pCount(pBuffer))--;
   printf("SUCCESS: %s was removed.\n", (char *)pTemp);
 }
 
 void searchPerson(void *pBuffer) {
   //   Check if pCount is equal to 0
-  if ((*((int *)pBuffer + 1)) == 0) {
+  if (*pCount(pBuffer) == 0) {
     printf("ERROR: Agenda is empty\n");
     return;
   }
@@ -173,23 +196,19 @@ void searchPerson(void *pBuffer) {
   scanf(" %[^\n]", (char *)pTemp);
 
   //   for(int i = 0; i< count;i++);
-  for (*((int *)pBuffer + 3) = 0;
-       (*((int *)pBuffer + 3)) < (*((int *)pBuffer + 1));
-       (*((int *)pBuffer + 3))++) {
-    if (strcmp((char *)pTemp,
-               (char *)(pBuffer + INITIAL_SIZE +
-                        ((*((int *)pBuffer + 3)) * PERSON_SIZE))) == 0) {
+  for (*pI(pBuffer) = 0; *pI(pBuffer) < *pCount(pBuffer); (*pI(pBuffer))++) {
+    if (strcmp((char *)pTemp, (char *)(pBuffer + INITIAL_SIZE +
+                                       ((*pI(pBuffer)) * PERSON_SIZE))) == 0) {
       printf("Name: ");
-      printf("%s\n", (char *)(pBuffer + INITIAL_SIZE +
-                              ((*((int *)pBuffer + 3)) * PERSON_SIZE)));
-      printf("E-mail: ");
       printf("%s\n",
-             (char *)(pBuffer + INITIAL_SIZE +
-                      ((*((int *)pBuffer + 3)) * PERSON_SIZE) + NAME_SIZE));
+             (char *)(pBuffer + INITIAL_SIZE + ((*pI(pBuffer)) * PERSON_SIZE)));
+      printf("E-mail: ");
+      printf("%s\n", (char *)(pBuffer + INITIAL_SIZE +
+                              ((*pI(pBuffer)) * PERSON_SIZE) + NAME_SIZE));
       printf("Age: ");
-      printf("%d\n", *((int *)(pBuffer + INITIAL_SIZE +
-                               ((*((int *)pBuffer + 3)) * PERSON_SIZE) +
-                               NAME_SIZE + EMAIL_SIZE)));
+      printf("%d\n",
+             *((int *)(pBuffer + INITIAL_SIZE + ((*pI(pBuffer)) * PERSON_SIZE) +
+                       NAME_SIZE + EMAIL_SIZE)));
       printf("\n");
       return;
     }
@@ -198,27 +217,24 @@ void searchPerson(void *pBuffer) {
 }
 void listAgenda(void *pBuffer) {
   //   Check if pCount is equal to 0
-  if ((*((int *)pBuffer + 1)) == 0) {
+  if (*pCount(pBuffer) == 0) {
     printf("ERROR: Agenda is empty\n");
     return;
   }
 
-  printf("\t AGENDA LIST (%d)\n", *((int *)pBuffer + 1));
+  printf("\t AGENDA LIST (%d)\n", *pCount(pBuffer));
   //   for(int i = 0; i< count;i++);
-  for (*((int *)pBuffer + 3) = 0;
-       (*((int *)pBuffer + 3)) < (*((int *)pBuffer + 1));
-       (*((int *)pBuffer + 3))++) {
+  for (*pI(pBuffer) = 0; (*pI(pBuffer)) < *pCount(pBuffer); (*pI(pBuffer))++) {
     printf("Name: ");
-    printf("%s\n", (char *)(pBuffer + INITIAL_SIZE +
-                            ((*((int *)pBuffer + 3)) * PERSON_SIZE)));
-    printf("E-mail: ");
     printf("%s\n",
-           (char *)(pBuffer + INITIAL_SIZE +
-                    ((*((int *)pBuffer + 3)) * PERSON_SIZE) + NAME_SIZE));
+           (char *)(pBuffer + INITIAL_SIZE + ((*pI(pBuffer)) * PERSON_SIZE)));
+    printf("E-mail: ");
+    printf("%s\n", (char *)(pBuffer + INITIAL_SIZE +
+                            ((*pI(pBuffer)) * PERSON_SIZE) + NAME_SIZE));
     printf("Age: ");
-    printf("%d\n", *((int *)(pBuffer + INITIAL_SIZE +
-                             ((*((int *)pBuffer + 3)) * PERSON_SIZE) +
-                             NAME_SIZE + EMAIL_SIZE)));
+    printf("%d\n",
+           *((int *)(pBuffer + INITIAL_SIZE + ((*pI(pBuffer)) * PERSON_SIZE) +
+                     NAME_SIZE + EMAIL_SIZE)));
     printf("\n");
   }
 }
